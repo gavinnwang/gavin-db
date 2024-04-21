13bd9945cbc6c4434f25c103c98da6fc5995896c
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-04-21 18:40:08 -0500

    kv

--- src/include/storage/kv_page.hpp
+++ src/include/storage/kv_page.hpp
@@ -9,14 +9,18 @@ public:
   auto GetNumKeys() const -> uint32_t {return num_keys_;}
   void Get(const std::string& key,std::string* value) const;
   void Put(const std::string& key, const std::string& value);
-  auto GetNextTupleOffset(const std::string& key, const std::string& value);
+  auto GetNextTupleOffset(const std::string& key, const std::string& value) const -> std::optional<uint16_t>;
 private:
-  uint32_t num_keys_;
   // offset + size
   using KVInfo = std::tuple<uint16_t, uint16_t>;
   KVInfo kv_info_[0];
-  uint16_t num_tuples_;
-  static constexpr size_t KV_INFO_SIZE = 4;
+  uint16_t num_keys_;
+
+  static constexpr uint16_t KV_INFO_SIZE = 4;
+
   static_assert(sizeof(KVInfo) == KV_INFO_SIZE);
 };
+
+  static constexpr uint16_t KV_PAGE_HEADER_SIZE = 2;
+  static_assert(sizeof(KVPage) == KV_PAGE_HEADER_SIZE);
 }
--- src/storage/kv_page.cpp
+++ src/storage/kv_page.cpp
@@ -1,5 +1,10 @@
 #pragma once
 #include "storage/kv_page.hpp"
+#include "storage/disk.hpp"
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <string>
 namespace db{
 void KVPage::Init(){
   num_keys_ = 0; 
@@ -9,5 +14,36 @@ void KVPage::Get(const std::string& key, std::string* value) const {
   return;  
 }
 
-  auto KVPage::GetNextTupleOffset(const TupleMeta &meta, const Tuple &tuple) const -> std::optional<uint16_t>;
+  void KVPage::Put(const std::string& key, const std::string& value){
+
+  auto offset = GetNextTupleOffset(key, value);
+  if (offset == std::nullopt) {
+   return; 
+  }
+  uint16_t kv_size = key.size() + value.size();
+  kv_info_[num_keys_] = std::make_tuple(*offset, kv_size);
+  num_keys_++;
+  memcpy(page_start_ + *tuple_offset, tuple.data_.data(), tuple.GetLength());
+  return tuple_id;
+
+
+}
+
+auto KVPage::GetNextTupleOffset(const std::string& key, const std::string& value) const -> std::optional<uint16_t> {
+  size_t end_offset;
+  if (num_keys_ > 0) {
+    auto &[offset, size] = kv_info_[num_keys_ - 1];
+    end_offset = offset;
+  } else {
+    end_offset = PAGE_SIZE;
+  }
+  auto kv_size = key.size() + value.size();
+  auto kv_offset = end_offset - kv_size;
+  auto offset_size = KV_PAGE_HEADER_SIZE + KV_INFO_SIZE * (num_keys_ + 1);
+
+  if (kv_offset < offset_size) {
+    return std::nullopt;
+  }
+  return kv_offset;
+}
 }
