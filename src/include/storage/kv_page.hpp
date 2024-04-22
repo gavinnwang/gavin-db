9c59024364ed075d9effb954dbbe6fee178bd724
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-04-21 20:18:46 -0500

    kv

--- src/include/storage/kv_page.hpp
+++ src/include/storage/kv_page.hpp
@@ -3,6 +3,15 @@
 #include <cstdint>
 #include <string>
 namespace db {
+static constexpr uint16_t KV_META_SIZE = 4;
+struct KVMeta {
+  bool is_deleted_;
+  uint16_t key_size_;
+};
+static_assert(sizeof(KVMeta) == KV_META_SIZE);
+
+static constexpr uint64_t KV_PAGE_HEADER_SIZE = 2;
+
 class KVPage {
 public:
   void Init();
@@ -10,17 +19,17 @@ public:
   void Get(const std::string& key,std::string* value) const;
   void Put(const std::string& key, const std::string& value);
   auto GetNextTupleOffset(const std::string& key, const std::string& value) const -> std::optional<uint16_t>;
+  void PrintContent() const;
 private:
   // offset + size
-  using KVInfo = std::tuple<uint16_t, uint16_t>;
+  using KVInfo = std::tuple<uint16_t, uint16_t, KVMeta>;
+  char page_start_[0];
   KVInfo kv_info_[0];
   uint16_t num_keys_;
-
-  static constexpr uint16_t KV_INFO_SIZE = 4;
-
+  
+  static constexpr uint16_t KV_INFO_SIZE = 2 + KV_PAGE_HEADER_SIZE + KV_META_SIZE;
   static_assert(sizeof(KVInfo) == KV_INFO_SIZE);
 };
 
-  static constexpr uint16_t KV_PAGE_HEADER_SIZE = 2;
   static_assert(sizeof(KVPage) == KV_PAGE_HEADER_SIZE);
 }
--- src/storage/kv_page.cpp
+++ src/storage/kv_page.cpp
@@ -3,41 +3,48 @@
 #include "storage/disk.hpp"
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
+#include <iostream>
 #include <optional>
 #include <string>
+#include <vector>
 namespace db{
 void KVPage::Init(){
   num_keys_ = 0; 
 }
 
 void KVPage::Get(const std::string& key, std::string* value) const {
+  
   return;  
 }
 
-  void KVPage::Put(const std::string& key, const std::string& value){
-
+  
+void KVPage::Put(const std::string& key, const std::string& value) {
   auto offset = GetNextTupleOffset(key, value);
   if (offset == std::nullopt) {
    return; 
   }
   uint16_t kv_size = key.size() + value.size();
-  kv_info_[num_keys_] = std::make_tuple(*offset, kv_size);
+  if (key.size() > std::numeric_limits<uint16_t>::max()) {
+    std::cout << "not allowed to have a key greater than max uint16" << std::endl;
+    return;
+  } 
+  auto meta = KVMeta{false, static_cast<uint16_t>(key.size())};
+  kv_info_[num_keys_] = std::make_tuple(*offset, kv_size, meta);
   num_keys_++;
-  memcpy(page_start_ + *tuple_offset, tuple.data_.data(), tuple.GetLength());
-  return tuple_id;
-
-
+  memcpy(page_start_ + *offset, key.data(), key.size());
+  memcpy(page_start_ + *offset + key.size(), key.data(), key.size());
 }
 
 auto KVPage::GetNextTupleOffset(const std::string& key, const std::string& value) const -> std::optional<uint16_t> {
   size_t end_offset;
   if (num_keys_ > 0) {
-    auto &[offset, size] = kv_info_[num_keys_ - 1];
+    auto &[offset, size, meta] = kv_info_[num_keys_ - 1];
     end_offset = offset;
   } else {
     end_offset = PAGE_SIZE;
   }
-  auto kv_size = key.size() + value.size();
+  uint16_t kv_size = key.size() + value.size();
   auto kv_offset = end_offset - kv_size;
   auto offset_size = KV_PAGE_HEADER_SIZE + KV_INFO_SIZE * (num_keys_ + 1);
 
@@ -45,5 +52,15 @@ auto KVPage::GetNextTupleOffset(const std::string& key, const std::string& value
     return std::nullopt;
   }
   return kv_offset;
+}
+
+void KVPage::PrintContent() const {
+  for (const auto& [offset, size, meta] : kv_info_) {
+    std::vector<char> data;
+    std::cout<< "[" << offset << ", " << size << ", " << meta.key_size_ << "]" << std::endl;
+    memmove(data.data(), page_start_ + offset, size);
+    std::string str(data.begin(), data.end()); 
+    std:: cout << str << std::endl;
+  }
 }
 }
