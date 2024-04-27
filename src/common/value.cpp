252158a5c24ebb84c7cd58c162046f8986b2643a
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2024-04-27 15:06:59 -0500

    value

--- src/common/value.cpp
+++ src/common/value.cpp
@@ -3,6 +3,63 @@
 #include "common/type.hpp"
 #include <cstdint>
 namespace db {
+Value::Value(TypeId type_id, const char *data, uint32_t var_len,
+             bool manage_data) {
+  switch (type_id) {
+  case TypeId::VARCHAR:
+    manage_data_ = manage_data;
+    if (manage_data_) {
+      assert(var_len < 50);
+      value_.varlen_ = new char[var_len];
+      assert(value_.varlen_ != nullptr);
+      var_len_ = var_len;
+      memcpy(value_.varlen_, data, var_len);
+    } else {
+      // don't manage our data
+      value_.const_varlen_ = data;
+      var_len_ = var_len;
+    }
+    break;
+  default:
+    UNREACHABLE("Invalid Type for variable-length Value constructor");
+  }
+}
+// copy constructor
+Value::Value(const Value &other) {
+  type_id_ = other.type_id_;
+  var_len_ = other.var_len_;
+  manage_data_ = other.manage_data_;
+  value_ = other.value_;
+  switch (type_id_) {
+  case TypeId::VARCHAR:
+    if (manage_data_) {
+      value_.varlen_ = new char[var_len_];
+      memcpy(value_.varlen_, other.value_.varlen_, var_len_);
+    } else {
+      value_ = other.value_;
+    }
+    break;
+  default:
+    value_ = other.value_;
+  }
+};
+// copy assignment operator
+auto Value::operator=(Value other) -> Value & {
+  Swap(*this, other);
+  return *this;
+}
+// deconstructor
+Value::~Value() {
+  switch (type_id_) {
+  case TypeId::VARCHAR:
+    if (manage_data_) {
+      delete[] value_.varlen_;
+    }
+    break;
+  default:
+    break;
+  }
+}
 void Value::SerializeTo(char *storage) const {
   switch (type_id_) {
   case TypeId::BOOLEAN: {
--- src/include/common/value.hpp
+++ src/include/common/value.hpp
@@ -8,26 +8,36 @@ class Value {
 public:
   Value() : type_id_(TypeId::INVALID), var_len_(INVALID_VAR_LEN) {};
 
-    // create an BOOLEAN value
-    Value(TypeId type, int8_t i) : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.boolean_ = i} {}
-    
-    // INTEGER
-    Value(TypeId type, int32_t i) : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.integer_ = i} {}
-    
-    // TIMESTAMP
-    Value(TypeId type, uint64_t i) : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.timestamp_ = i} {}
+  // create an BOOLEAN value
+  Value(TypeId type, int8_t i)
+      : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.boolean_ = i} {}
+
+  // INTEGER
+  Value(TypeId type, int32_t i)
+      : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.integer_ = i} {}
+
+  // TIMESTAMP
+  Value(TypeId type, uint64_t i)
+      : type_id_(type), var_len_(INVALID_VAR_LEN), value_{.timestamp_ = i} {}
   // VARCHAR
-  Value(TypeId type, const char *data, uint32_t len, bool manage_data);
+  Value(TypeId type_id, const char *data, uint32_t len, bool manage_data);
   // copy constructor
   Value(const Value &other);
-  // move constructor
-  Value(Value &&other) noexcept;
+  // // move constructor
+  // Value(Value &&other) noexcept;
   // deconstructor
   ~Value();
   // copy assignment
-  Value &operator=(const Value &other);
-  // move assignment
-  Value &operator=(Value &&other) noexcept;
+  auto operator=(Value other) -> Value &;
+  // // move assignment
+  // auto operator=(Value &&other) noexcept -> Value &;
+
+  friend void Swap(Value &first, Value &second) {
+    std::swap(first.value_, second.value_);
+    std::swap(first.var_len_, second.var_len_);
+    std::swap(first.manage_data_, second.manage_data_);
+    std::swap(first.type_id_, second.type_id_);
+  }
 
   // Get the length of the variable length data
   inline auto GetStorageSize() const -> uint32_t {
@@ -64,7 +74,7 @@ public:
       return {type_id, storage + sizeof(uint32_t), var_len, true};
     }
     case TypeId::INVALID: {
-        UNREACHABLE("Value has invalid type id");
+      UNREACHABLE("Value has invalid type id");
     }
     }
   }
@@ -75,11 +85,13 @@ private:
 
   uint32_t var_len_;
 
+  bool manage_data_;
+
   union Val {
     int8_t boolean_;
     int32_t integer_;
     uint64_t timestamp_;
-    char* varlen_;
+    char *varlen_;
     const char *const_varlen_;
   } value_;
 };
