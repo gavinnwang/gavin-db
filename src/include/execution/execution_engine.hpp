c2bbfe2f0e0c8360c5ed11e3c20fdc429e4fca8c
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-07-26 10:29:30 -0700

    Poll executor

--- src/include/execution/execution_engine.hpp
+++ src/include/execution/execution_engine.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "common/rid.hpp"
+#include "execution/executors/abstract_executor.hpp"
 #include "execution/plans/abstract_plan.hpp"
+#include "storage/table/tuple.hpp"
 namespace db {
 
 class ExecutionEngine {
@@ -10,6 +13,13 @@ public:
 	DISALLOW_COPY_AND_MOVE(ExecutionEngine);
 
 private:
+	static void PollExecutor(std::unique_ptr<AbstractExecutor> &executor, std::vector<Tuple> &result_set) {
+		RID rid {};
+		Tuple tuple {};
+		while (executor->Next(tuple, rid)) {
+			result_set.push_back(tuple);
+		}
+	}
 	AbstractPlanNodeRef plan_;
 };
 
