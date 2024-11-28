e2dc1966f6e2895eea7068db41db66d885d24cff
Gavin <79772347+gavinnwang@users.noreply.github.com>
2024-11-28 11:22:37 -0800

    create statement

--- src/include/binder/statement/bound_statement.hpp
+++ src/include/binder/statement/bound_statement.hpp
@@ -1,9 +1,20 @@
 #pragma once
+#include "binder/statement/statement_type.hpp"
+#include "common/exception.hpp"
 namespace db {
 
 class BoundStatement {
 public:
 	explicit BoundStatement(StatementType type);
 	virtual ~BoundStatement() = default;
+
+	/** The statement type. */
+	StatementType type_;
+
+public:
+	/** Render this statement as a string. */
+	virtual auto ToString() const -> std::string {
+		throw Exception("ToString not supported for this type of SQLStatement");
+	}
 };
 } // namespace db
--- src/include/binder/statement/create_statement.hpp
+++ src/include/binder/statement/create_statement.hpp
@@ -1,5 +1,16 @@
 #pragma once
+#include "binder/statement/bound_statement.hpp"
+#include "catalog/column.hpp"
 namespace db {
 
-class CreateStatement : public BoundStatement {};
+class CreateStatement : public BoundStatement {
+
+	explicit CreateStatement(std::string table, std::vector<Column> columns, std::vector<std::string> primary_key);
+
+	std::string table_;
+	std::vector<Column> columns_;
+	std::vector<std::string> primary_key_;
+
+	auto ToString() const -> std::string override;
+};
 } // namespace db
